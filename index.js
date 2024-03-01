521ed2f3e02e5108dfc804ea28d6b9cd98569063
is0280fp <umeyume45@gmail.com>
2024-03-02 08:20:31 +0900

    Add section visibility animation

---
+++ index.js
@@ -0,0 +1,22 @@
+const sections = document.querySelectorAll("section")
+
+window.addEventListener("scroll", () => {
+    animationByScroll()
+})
+
+function animationByScroll() {
+    // 今映っている画面に辿り着くまでにスクロールした量
+    const scrollY = window.scrollY
+    // 今映っている画面の高さ
+    const windowHeidht = window.innerHeight
+    //ずらす用の高さ
+    const INVIEW_HEIGHT = 200
+
+    sections.forEach((section) => {
+        const offsetTop = section.offsetTop
+
+        if (scrollY + windowHeidht > offsetTop + INVIEW_HEIGHT) {
+            section.classList.add("SectionIsActive")
+        }
+    })
+}
